9643afb497a3c17b9e055a9bd5118fb4e3388674
Gus1101 <augustosantosb60@gmail.com>
2024-09-25 00:16:53 -0300

    mudando bucket

--- data/ml_data.py
+++ data/ml_data.py
@@ -3,5 +3,5 @@ from connections.send_aws_s3 import upload_file_to_s3
 #Carrega a base para a camada gold no S3
 upload_file_to_s3(
     file_path="transient/ml_enhanced_gold.parquet",
-    bucket_name="golddatafiapec",
-    object_name="ml_enhanced_gold.parquet")
+    bucket_name="mllayerbucket",
+    object_name="ml_enhanced.parquet")
