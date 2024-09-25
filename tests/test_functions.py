0bbe9cdad93ab2aa496a146316323f9d5b2a5336
Gus1101 <augustosantosb60@gmail.com>
2024-09-25 14:43:02 -0300

    criando um teste para falhar

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,5 +4,5 @@ from data.functions_data import round_datetime_to_nearest_hour
 
 def test_round_down():
     dt = datetime(2023, 9, 24, 14, 15)
-    expected = datetime(2023, 9, 24, 14, 0)
+    expected = datetime(2023, 9, 24, 14, 15)
     assert round_datetime_to_nearest_hour(dt) == expected
